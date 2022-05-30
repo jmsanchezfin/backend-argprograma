fe2e718cf7aa2ca3d3b5b55ed7dca3870df2e6db
JMSF <jmsanchezfin@gmail.com>
2022-05-30 19:44:48 -0300

    Added CROS to controllers, and dependencies

--- portfolio/src/main/java/com/proyecto/portfolio/controller/ExperienceController.java
+++ portfolio/src/main/java/com/proyecto/portfolio/controller/ExperienceController.java
@@ -4,6 +4,7 @@ import com.proyecto.portfolio.entity.Experience;
 import com.proyecto.portfolio.service.IExperienceService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ExperienceController {
     
